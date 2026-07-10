a8ed741c0027e6483294da7e55745f079aabea2e
XLex0 <alexander.redes17@gmail.com>
2026-07-09 23:17:42 -0500

    Added: Añadido Level Selector

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,25 +1,51 @@
-extends Control # Si tu nodo MainMenu es Node2D, cambia esto a "extends Node2D"
+extends Control 
 
-# 1. Actualizamos las rutas agregando CanvasLayer al inicio
-@onready var btn_jugar = $CanvasLayer/Play     # Usé los nombres de tu captura anterior
+# --- RUTA DE BOTONES PRINCIPALES
+@onready var btn_jugar = $CanvasLayer/Play
 @onready var btn_opciones = $CanvasLayer/Options
-@onready var btn_salir = $CanvasLayer/Exit
+@onready var btn_salir_principal = $CanvasLayer/Exit
+
+# --- CONTENEDOR DE NIVELES 
+@onready var menu_niveles = $Level
+@onready var btn_nivel_1 = $Level/One
+@onready var btn_nivel_2 = $Level/Two
+@onready var btn_nivel_3 = $Level/Three
+@onready var btn_salir_niveles = $Level/Exit
 
 func _ready():
-	# Conectar las señales de click
+	# niveles ocultos al inicio
+	menu_niveles.visible = false
+	
+	# 2. Conectar señales del menú principal
 	btn_jugar.pressed.connect(_on_btn_jugar_pressed)
 	btn_opciones.pressed.connect(_on_btn_opciones_pressed)
-	btn_salir.pressed.connect(_on_btn_salir_pressed)
+	btn_salir_principal.pressed.connect(_on_btn_salir_principal_pressed)
 	
+	# 3. Conectar señales del menú de niveles
+	btn_nivel_1.pressed.connect(_on_nivel_seleccionado)
+	btn_nivel_2.pressed.connect(_on_nivel_seleccionado)
+	btn_nivel_3.pressed.connect(_on_nivel_seleccionado)
+	btn_salir_niveles.pressed.connect(_on_btn_salir_niveles_pressed)
 
 
-# --- LÓGICA DE CLICKS ---
+
 func _on_btn_jugar_pressed():
-	# Cambia "res://planta_baja.tscn" por la ruta real de tu escena de juego
-	get_tree().change_scene_to_file("res://planta_baja.tscn")
+	# En lugar de cambiar de escena directo, mostramos el menú de niveles
+	menu_niveles.visible = true
 
 func _on_btn_opciones_pressed():
 	print("Abrir panel de opciones (pendiente de implementar)")
 
-func _on_btn_salir_pressed():
+func _on_btn_salir_principal_pressed():
 	get_tree().quit()
+
+
+# --- LÓGICA DEL MENÚ DE NIVELES ---
+
+func _on_nivel_seleccionado():
+	# Redirige a la planta baja (por ahora todos) 
+	get_tree().change_scene_to_file("res://planta_baja.tscn")
+
+func _on_btn_salir_niveles_pressed():
+	# Oculta el menú de niveles y vuelve a dejar interactuable el menú principal
+	menu_niveles.visible = false
